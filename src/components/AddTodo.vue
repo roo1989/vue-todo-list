d003df7bad5141917a30dd8a53610c84737f085c
roo1989 <ragnar.orn@gmail.com>
2020-08-10 19:32:18 +0000

    Add event handler to add todos, and construct the object in the AddTodo component

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,15 +1,37 @@
 <template>
     <div>
-        <form>
-            <input type="text" name="title" placeholder="Add Todo...">     
+        <form @submit="addTodo">
+            <input type="text" v-model="title" name="title" placeholder="Add Todo...">     
             <input type="submit" value="Submit" class="btn">
         </form>    
     </div>    
 </template>
 
 <script>
+import uuid from 'uuid';
+
 export default {
    name: "AddTodo",
+   data() {
+       return {
+           title: '',
+       }
+   },
+   methods: {
+       addTodo(e) {
+           e.preventDefault();
+           const newTodo = {
+               id: uuid,
+               title: this.title,
+               completed: false,
+           }
+           // Add event to parent.
+           this.$emit('add-todo', newTodo);
+
+           // Clear title input
+           this.title = '';
+       }
+   }
 }
 </script>
 
